711592613e6ad28c1d10f553414faddca9b1ca81
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-05-17 20:27:04 +0300

    Add project files.

---
+++ Services/ContactAPI/Contact.API.Services/ContactService.cs
@@ -0,0 +1,23 @@
+﻿using Contact.API.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.API.Services
+{
+    public class ContactService : IContactService
+    {
+        public ContactDto GetContactById(int id)
+        {
+            return new ContactDto()
+            {
+                Id = id,
+                FirstName = "Serhat",
+                LastName = "Kaçmaz"
+            };
+        }
+
+    }
+}
---
+++ Services/ContactAPI/Contact.API/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Contact.API.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contact.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            this.contactService = contactService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(contactService.GetContactById(id));
+        }
+    }
+}
